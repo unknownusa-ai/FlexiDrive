74b0c8454e4c52388967f99981d18655e10d1e80
GabrielP1711 <gaps.admon17@gmail.com>
2026-05-11 00:05:19 -0500

    frame drops eliminados

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,21 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flexidrive/app.dart';
-import 'package:flexidrive/injection_container.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(const App());
-
-  unawaited(_warmUpDependencies());
-}
-
-Future<void> _warmUpDependencies() async {
-  try {
-    await InjectionContainer.instance.warmUp();
-  } catch (error) {
-    debugPrint('FlexiDrive startup cache skipped: $error');
-  }
 }
